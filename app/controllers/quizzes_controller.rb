9c41a561f9792178c7f637c55ce3cc4e86748152
xavier delarue <xdelarue33@gmail.com>
2019-11-26 17:53:40 +0100

    Add relation question-option, seed

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,19 @@
 class QuizzesController < ApplicationController
   def show
+    # Check quiz in progress
+    quiz = user_session[:quiz]
+    if quiz == nil 
+      # Pick random one quiz
+      @quiz = Quiz.all.sample
+      quizSession = {
+        id: @quiz.id
+        score: 0
+        currentQuestion: 1
+      }
+      # Start new quiz
+      user_session[:quiz] = quizSession
+    else
+      @quiz = Quiz.find(quiz[:id])
+    end
   end
 end
