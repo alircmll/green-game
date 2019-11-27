76f461fd66dc66050c5bfc6d82e2b537f3026686
KevinLamSeck <kevin.lamseck@gmail.com>
2019-11-27 12:24:34 +0100

    formulaire edit in profil

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'categories#index'
   resources :categories, only: [:show]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update, :edit]
   resources :challenges, only: [:index, :show, :new, :create, :update] do
     resources :party_challenges, only: [:create]
   end
