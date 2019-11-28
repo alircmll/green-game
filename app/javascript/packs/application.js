acc03b743a867f22fe9c6addabc3d41db8ac3df8
xavier delarue <xdelarue33@gmail.com>
2019-11-28 18:29:01 +0100

    Refact seed & debugg application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,9 @@ import "bootstrap";
 import $ from "jquery";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import { flipcard } from '../components/flipcard';
 import { collapse } from "plugins/collapsible.js";
-
-flipcard();
-collapse();
-
 import { flipcard } from '../components/flipcard';
-
 import { initMapbox } from '../plugins/init_mapbox';
-
 import { closeNav, openNav} from  "plugins/navbottom.js";
 
 // MENU
@@ -19,4 +12,4 @@ $("#userMenuBtn").click(openNav);
 $("#closeUserBtn").click(closeNav);
 initMapbox();
 flipcard();
-
+collapse();
