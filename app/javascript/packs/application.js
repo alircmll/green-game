4ce1f18967604a703089fdcf075bf8b4d4236d62
alice roycamille <alice_rc@live.fr>
2019-11-29 14:30:56 +0100

    fix js bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,17 +4,18 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import $ from "jquery";
 import { collapse } from "plugins/collapsible.js";
 import { flipcard } from '../components/flipcard';
-import { confetti } from '../components/confetti';
+// import { confetti } from '../components/confetti';
 import { initMapbox } from '../plugins/init_mapbox';
 import { closeNav, openNav} from  "plugins/navbottom.js";
 import '../plugins/fadeinscrool'
 
 // MENU
 document.getElementById(userMenuBtn)
-
+// const confet = document.getElementById('confetti')
+// confet.addEventListener("load", confetti());
 $("#userMenuBtn").click(openNav);
 $("#closeUserBtn").click(closeNav);
 initMapbox();
 flipcard();
 collapse();
-confetti();
+// confetti();
