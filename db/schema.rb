671481bdfc272bb0fb929274bd9e205e048f07e5
Florent GONON <florent.gonon33@gmail.com>
2019-11-29 11:51:18 +0100

    add photo to categories seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_134735) do
+ActiveRecord::Schema.define(version: 2019_11_29_102348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_11_28_134735) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "challenges", force: :cascade do |t|
