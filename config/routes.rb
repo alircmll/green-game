b06a7c24175ec4ff0163194cd54a437ea556a2ad
KevinLamSeck <kevin.lamseck@gmail.com>
2019-12-02 16:07:27 +0100

    route 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
+  get '*unmatched_route', :to => 'errors#not_found'
 
   devise_for :users
   root to: 'categories#index'
