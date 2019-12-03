a85c3050be508a0b9874c9ba3c6dffc9b694e388
xavier delarue <xdelarue33@gmail.com>
2019-12-03 12:10:59 +0100

    add details and links to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2019_12_02_094154) do
-
+ActiveRecord::Schema.define(version: 2019_12_03_104900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +99,8 @@ ActiveRecord::Schema.define(version: 2019_12_02_094154) do
     t.bigint "quiz_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "details"
+    t.string "link"
     t.index ["quiz_id"], name: "index_questions_on_quiz_id"
   end
 
