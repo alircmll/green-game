45dda2d7f7b6b4cdaace1202c6aa5d380c3f288c
KevinLamSeck <kevin.lamseck@gmail.com>
2019-12-03 18:17:00 +0100

    Add Completed boolean to Party Challenge

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2019_12_02_094154) do
-
+ActiveRecord::Schema.define(version: 2019_12_03_171156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_094154) do
     t.bigint "challenge_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
     t.index ["challenge_id"], name: "index_party_challenges_on_challenge_id"
     t.index ["user_id"], name: "index_party_challenges_on_user_id"
   end
