622dab26fb5191b1130d4b21f053575afff6359d
KevinLamSeck <kevin.lamseck@gmail.com>
2019-12-04 16:49:38 +0100

    User Show message par default

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
-    # @party_challenges = PartyChallenge.where(user_id: current_user)
     # ! Voir si une partie n'est pas completed
     @party_challenges = PartyChallenge.where(completed: false, user_id: current_user)
-    # @challenge = Challenge.find(params[:id])
-
-    # Si la tache est faite rajouter des points
-    # if @party_challenges.updated_at.day != Time.now.day
-    #   Reecrire le completed en false si la date est differente du jour, et limiter sur la duration
-    #   @party_challenge.update(completed: false)
-    #   @party_challenge.save
-    # end
+    @count = 0
+    @countdone = 0
+    @countcat = 0
+    @party_challenges.each do |pc|
+      if (pc.updated_at.day != Time.now.day) || (pc.created_at.day == Time.now.day and pc.position == 0)
+        @count += 1
+      end
+      if (pc.updated_at.day == Time.now.day and pc.position > 0)
+        @countdone += 1
+      end
+    end
 
     # * J'appelle la function Level Up et je sauvegarde dans la DB
     level_up
@@ -20,7 +22,6 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(current_user.id)
-
   end
 
   def update
